7af8d9a813bf12594d545b4b563cb3b973fec66b
bhaskar361-kreeti <bhaskar.chatterjee@kreeti.com>
2025-08-22 13:20:54 +0530

    Movie List Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
 import {Routes, Route} from 'react-router-dom'
 import MovieList from './Components/MovieList'
+import MovieDetails from './Components/MovieDetails'
 
 function App() {
 
 
   return (
     <>
+    <div className='bg-black p-10'>
       <Routes>
         <Route path='/' element={<MovieList/>} />
+        <Route path='/movie' element={<MovieDetails/>} />
+
       </Routes>
+    </div>
     </>
   )
 }
